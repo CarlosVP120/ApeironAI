0eb05ad0289443ae04602e42fdbae20b10da0377
CarlosVP120 <carlosveryan4@gmail.com>
2022-12-30 17:19:26 -0600

    Cleanup

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fadcd7e518f70155"; }
+/******/ 		__webpack_require__.h = function() { return "a5e9f37a4451af3d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/homepage/home5-dark.jsx
+++ src/pages/homepage/home5-dark.jsx
@@ -5,6 +5,7 @@ import CallToAction from "../../components/Call-to-action/call-to-action";
 import Footer from "../../components/Footer/footer";
 import Team from "../../components/Team/team";
 import DarkTheme from "../../layouts/Dark";
+import LightTheme from "../../layouts/Light";
 import Works3 from "../../components/Works3/works3";
 import Blogs4 from "../../components/blogs/Blogs4/blogs4";
 import Intro4 from "../../components/Intro4/intro4";
@@ -34,8 +35,10 @@ const Homepage5 = () => {
   }, [navbarRef]);
   return (
     <DarkTheme>
+      {/* <Navbar nr={navbarRef} lr={logoRef} theme="themeL" /> FOR LIGHT THEME  */}
       <Navbar nr={navbarRef} lr={logoRef} />
       <Intro4 />
+      {/* <Intro4 blackStar /> FOR LIGHT THEME*/}
       <AboutUs3 />
       <Works3 />
       <Services3 />
