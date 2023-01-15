d8abbc8f75ee7cc92165d00193f6436843154e5b
Diego <diegopartidaromero@gmail.com>
2023-01-15 02:12:15 -0600

    Generate Code

--- src/components/GenerateCode.jsx
+++ src/components/GenerateCode.jsx
@@ -1,8 +1,60 @@
+import { useCallback } from "react";
 import { useState } from "react";
+import Loading from "./Loading";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/themes/prism.css";
 
 export default function GenerateCode() {
+  const code = ``;
 
-  const [inputText, setInputText] = useState("");
+  const [value, setValue] = useState(code);
+  const [prompt, setPrompt] = useState("");
+  const [completion, setCompletion] = useState("");
+  const [language, setLanguage] = useState("javascript");
+
+  const handleTab = (e) => {
+    if (e.keyCode === 9) {
+      e.preventDefault();
+      const start = e.target.selectionStart;
+      const end = e.target.selectionEnd;
+      e.target.value =
+        e.target.value.substring(0, start) +
+        "\t" +
+        e.target.value.substring(end);
+      e.target.selectionStart = e.target.selectionEnd = start + 1;
+      setValue(e.target.value);
+    }
+  };
+
+  const handleInput = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const askName = "Generate a " + language + " code that: ";
+
+  const handleClick = useCallback(
+    async (e) => {
+      setPrompt(askName + value);
+      setCompletion("Loading...");
+      const response = await fetch("/api/hello", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text: askName + value }),
+      });
+      const data = await response.json().then((data) => {
+        setCompletion(data.result.choices[0].text);
+      });
+    },
+    [value]
+  );
+
+  const handleSelect = (e) => {
+    setLanguage(e.target.value);
+    console.log(e.target.value);
+  };
 
   return (
     <div className="tw-w-full tw-h-[100vh] tw-bg-black tw-text-white tw-flex tw-justify-center">
@@ -29,47 +81,85 @@ export default function GenerateCode() {
             <h2 className="tw-mt-1 tw-text-sm">
               Create new code based on the syntax provided.
             </h2>
-            <h2 className="tw-mt-4 ">What do you want to create?</h2>
-            <textarea className="tw-mt-4 tw-w-full tw-h-[80%] tw-p-4 tw-rounded-lg tw-text-black tw-bg-gray-300 tw-resize-none tw-transition tw-duration-500 focus:tw-outline-0" value={inputText} onChange={(e) => setInputText(e.target.value)} />
-            <button className="tw-mt-4 tw-w-full tw-p-4 tw-rounded-lg tw-text-black tw-bg-gray-100">
+            <h2 className="tw-mt-4 ">
+              {"Generate a " + language + " code that... "}
+            </h2>
+            <textarea
+              className="tw-mt-4 tw-w-full tw-h-[80%] tw-p-4 tw-rounded-lg tw-text-[#eeffff] tw-bg-neutral-800 tw-border-black  tw-resize-none tw-transition tw-duration-500 focus:tw-outline-0 tw-font-code"
+              value={value}
+              onChange={handleInput}
+              onKeyDown={handleTab}
+              placeholder="Calculate the area of a circle with radius 5"
+            />
+            <button
+              onClick={handleClick}
+              className=" tw-mt-4 tw-w-full tw-p-4 tw-rounded-lg tw-text-white tw-bg-gray-100 tw-transition tw-duration-500 tw-bg-purple-500 hover:tw-bg-white hover:tw-text-black"
+            >
               Generate
             </button>
           </div>
           <div className="tw-bg-neutral-800 tw-py-8 tw-rounded-lg tw-text-left tw-text tw-px-8 tw-rounded-l-none tw-max-w-[65%] tw-h-[80%]">
-            <h1 className="tw-flex tw-items-center tw-text-2xl">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="tw-w-8 tw-h-8 tw-mr-2 tw-stroke-current tw-stroke-2"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5"
-                />
-              </svg>
-              Code generated
-            </h1>
-            <h2 className="tw-mt-1 tw-text-sm">
-              This is where your code will appear.
-            </h2>
-            <h2>
-              <pre className="tw-text-sm tw-mt-4 tw-text-gray-400 tw-overflow-auto">
-                <code className="tw-text-sm tw-mt-4 tw-text-gray-400">
-                  {`// Path: src\components\CmdAssistance.jsx
-export default function CmdAssitance() {
-    return (
-        <div className="tw-w-full tw-h-[100vh] tw-bg-black tw-text-white tw-flex tw-justify-center">
-
-        </div>
-    );
-}`}
-                </code>
-              </pre>
-            </h2>
+            {completion !== "Loading..." ? (
+              <>
+                <h1 className="tw-flex tw-items-center tw-text-2xl">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="tw-w-8 tw-h-8 tw-mr-2 tw-stroke-current tw-stroke-2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5"
+                    />
+                  </svg>
+                  Code generated
+                </h1>
+                <h2 className="tw-mt-1 tw-text-sm">
+                  This is where your code will appear.
+                </h2>
+                <h2 className="tw-mt-4 tw-w-full">
+                  Select the language you want to use.
+                </h2>
+                <div className="tw-w-full">
+                  <select
+                    className="tw-mt-4 tw-w-full tw-p-4 tw-rounded-lg tw-text-black tw-bg-gray-300 tw-resize-none tw-transition tw-duration-500 focus:tw-outline-0"
+                    onChange={handleSelect}
+                  >
+                    <option value="javascript">JavaScript</option>
+                    <option value="python">Python</option>
+                    <option value="java">Java</option>
+                    <option value="html">HTML</option>
+                    <option value="css">CSS</option>
+                    <option value="c">C</option>
+                    <option value="c++">C++</option>
+                    <option value="c#">C#</option>
+                    <option value="php">PHP</option>
+                    <option value="ruby">Ruby</option>
+                    <option value="swift">Swift</option>
+                    <option value="go">Go</option>
+                    <option value="rust">Rust</option>
+                    <option value="scala">Scala</option>
+                    <option value="dart">Dart</option>
+                    <option value="sql">SQL</option>
+                  </select>
+                </div>
+                {completion !== "" && (
+                  <div className="tw-flex tw-justify-center tw-self-center tw-mt-4 tw-overflow-hidden">
+                    <div className="tw-text-lg tw-h-full tw-overflow-y-auto tw-p-4 tw-text-white tw-bg-neutral-600 tw-rounded-lg tw-font-mono">
+                      {completion}
+                    </div>
+                  </div>
+                )}
+              </>
+            ) : completion === "Loading..." ? (
+              <div className="tw-flex tw-justify-center tw-h-full">
+                <Loading />
+              </div>
+            ) : null}
           </div>
         </div>
       </div>
