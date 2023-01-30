b1344847e5de2eadf48947598b1ab4ca0c6b7fd2
Carlos Veryan <66846209+CarlosVP120@users.noreply.github.com>
2023-01-30 10:48:14 -0600

    Update apeiron.jsx

--- src/pages/apeiron.jsx
+++ src/pages/apeiron.jsx
@@ -41,7 +41,8 @@ export default function ToolHomePage() {
     if (userIsFullStack || userIsFullStack === "") setAccess(userIsFullStack);
     if (
       auth.currentUser?.uid === "re0xUfkL31cDNYcbYCeIdnp1CU62" ||
-      auth.currentUser?.uid === "AZfy1rwg9Ia0qb4c6VcM5Jb8Jrd2"
+      auth.currentUser?.uid === "AZfy1rwg9Ia0qb4c6VcM5Jb8Jrd2" ||
+      auth.currentUser?.uid === "bOgJMKRQSkaS3Z1Ef1snw4AFqp23"
     ) {
       setAccess("fullstack");
     }
