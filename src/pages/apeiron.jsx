708a2f7d2c8a77da720ea39abca8280d5e81fbec
CarlosVP120 <carlosveryan4@gmail.com>
2023-02-02 16:11:05 -0600

    Free Period

--- src/pages/apeiron.jsx
+++ src/pages/apeiron.jsx
@@ -38,14 +38,15 @@ export default function ToolHomePage() {
   const userIsFullStack = useFullStackStatus(auth.currentUser);
 
   useEffect(() => {
-    if (userIsFullStack || userIsFullStack === "") setAccess(userIsFullStack);
-    if (
-      auth.currentUser?.uid === "re0xUfkL31cDNYcbYCeIdnp1CU62" ||
-      auth.currentUser?.uid === "AZfy1rwg9Ia0qb4c6VcM5Jb8Jrd2" ||
-      auth.currentUser?.uid === "bOgJMKRQSkaS3Z1Ef1snw4AFqp23"
-    ) {
-      setAccess("fullstack");
-    }
+    // if (userIsFullStack || userIsFullStack === "") setAccess(userIsFullStack);
+    // if (
+    //   auth.currentUser?.uid === "re0xUfkL31cDNYcbYCeIdnp1CU62" ||
+    //   auth.currentUser?.uid === "AZfy1rwg9Ia0qb4c6VcM5Jb8Jrd2" ||
+    //   auth.currentUser?.uid === "bOgJMKRQSkaS3Z1Ef1snw4AFqp23"
+    // ) {
+    //   setAccess("fullstack");
+    // }
+    setAccess("fullstack");
   }, [userIsFullStack]);
 
   if (auth.currentUser) {
