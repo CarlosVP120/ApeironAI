2fa05dee2159cdb9f22310f8f4cd7650ce215e34
CarlosVP120 <carlosveryan4@gmail.com>
2023-02-08 00:58:05 -0600

    error

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -6,11 +6,15 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function handler(req, res) {
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: req.body.text,
-    temperature: 1, // Higher values means the model will take more risks.
-    max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-  });
+  const completion = await openai
+    .createCompletion({
+      model: "text-davinci-003",
+      prompt: req.body.text,
+      temperature: 1, // Higher values means the model will take more risks.
+      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+    })
+    .catch((error) => {
+      console.log("error");
+    });
   res.status(200).json({ result: completion.data });
 }
